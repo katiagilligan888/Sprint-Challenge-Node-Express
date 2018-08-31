80ff6d2ec378139852e0d45095a4f2df5261747c
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-31 13:27:33 -0600

    Added the request for actions for specific project

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ server.get("/api/projects/:id", (req, res) => {
     })
 }); 
 
+server.get("/api/projects/:id/actions", (req,res) => {
+    const id = req.params.id; 
+    pModel.getProjectActions(id).then(projectActions => {
+        if(projectActions){
+            res.status(200).json(project); 
+        }else{
+            res.status(404).json({error: "The specified ID does not exist"})
+        }
+    }).catch(err => {
+        res.status(500).json({error:"Error retrieving the project from the database"})
+    })
+})
 server.post("/api/projects", (req,res) => {
     const data = req.body; 
     if(data.name && data.description){
