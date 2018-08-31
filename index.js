e65efdaac5f867c51056d3139d78442d1349ad79
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-31 13:20:57 -0600

    Added the actions CRUD operators

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = express();
 
 server.use(express.json());
 
+// ----------- PROJECTS CRUD OPERATORS --------------------------------------------
+
 server.get("/api/projects", (req, res) => {
     pModel.get().then(projects => {
         res.status(200).json(projects);
@@ -66,12 +68,62 @@ server.put("/api/projects/:id", (req,res) => {
 
 // -------------ACTION CRUD OPERATIONS -----------------------------------------------
 
+server.get("/api/actions", (req, res) => {
+    aModel.get().then(actions => {
+        res.status(200).json(actions);
+    }).catch(err => {
+        res.status(500).json({error: "Error retrieving the actions from the database"})
+    })
+}); 
 
+server.get("/api/actions/:id", (req, res) => {
+    const id = req.params.id;
+    aModel.get(id).then(action => {
+        if(action){
+            res.status(200).json(action); 
+        }else{
+            res.status(404).json({error: "The specified ID does not exist"})
+        }
+    }).catch(err => {
+        res.status(500).json({error: "Error retrieving the action from the database"})
+    })
+}); 
 
+server.post("/api/actions", (req,res) => {
+    const data = req.body; 
+    if(data.project_id && data.description && data.notes){
+        aModel.insert(data).then(newAction => {
+            res.status(201).json(newAction)
+        }).catch(err => {
+            res.status(500).json({error: "Error inserting new action in database"})
+        })
+    }else {
+        res.status(400).json({error: "Please insert project ID, notes,  and description in req body"})
+    }
+})
 
+server.delete("/api/actions/:id", (req, res) => {
+    const id = req.params.id; 
+    aModel.remove(id).then(count => {
+        if(count > 0){
+            res.status(200).json({message: "Successfully deleted the action"})
+        }
+    }).catch(err =>{
+        res.status(500).json({error: "There was an error deleting action from database"})
+    })
+}); 
 
-// Project CRUD Operations ============
-
+server.put("/api/actions/:id", (req,res) => {
+    const id = req.params.id; 
+    const updatedData = req.body; 
+    if(updatedData.project_id && updatedData.description && updatedData.notes){
+        aModel.update(id, updatedData).then(updated => {
+            res.status(200).json({updated})
+        }).catch(err => {
+            res.status(500).json({error: "There was an error updating action in database"})
+        })
+    }
+}); 
 
 
 server.listen(9000); 
